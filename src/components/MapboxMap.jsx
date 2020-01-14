96ba1897e70000c01d6a9b4b6c44cfd23c88f494
kwillard95 <kwillard95@gmail.com>
2020-01-13 19:29:35 -0800

    New user and existing user both route to a user dashboard

--- src/components/MapboxMap.jsx
+++ src/components/MapboxMap.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
+function Map(props) {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
 
-export default function MapContainer(props) {
-  mapboxgl.accessToken = 'pk.eyJ1Ijoia3dpbGxhcmQ5NSIsImEiOiJjazVjcm13dW8wM2Z0M2ZzZG00bnRqNDk2In0.-fxwhaz17ffbsqenvOkTMQ'
-
-  const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-  })
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={'pk.eyJ1Ijoia3dpbGxhcmQ5NSIsImEiOiJjazVjcm13dW8wM2Z0M2ZzZG00bnRqNDk2In0.-fxwhaz17ffbsqenvOkTMQ'}
+    />
+  );
 }
+
+export default Map;
